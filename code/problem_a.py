17712ac587c9aa774fdc7b13d225926b7f73b7e3
Heesom <heesom.hs@gmail.com>
2023-01-20 11:15:41 +0900

    feat: add movie_info in problem_a

--- code/problem_a.py
+++ code/problem_a.py
@@ -3,8 +3,16 @@ from pprint import pprint
 
 
 def movie_info(movie):
-    pass 
-    # 여기에 코드를 작성합니다.    
+    movie_shawshank = {}
+
+    movie_shawshank["id"] = movie.get("id")
+    movie_shawshank["title"] = movie.get("title")
+    movie_shawshank["poster_path"] = movie.get("poster_path")
+    movie_shawshank["vote_average"] = movie.get("vote_average")
+    movie_shawshank["overview"] = movie.get("overview")
+    movie_shawshank["genre_ids"] = movie.get("genre_ids")
+  
+    
 
 
 # 아래의 코드는 수정하지 않습니다.
@@ -13,3 +21,4 @@ if __name__ == '__main__':
     movie_dict = json.load(movie_json)
     
     pprint(movie_info(movie_dict))
+ 
